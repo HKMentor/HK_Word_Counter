8ac9c49aa04bd3aaf6792d02611aac506dfe34e0
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-04-23 20:57:32 +0500

    Project 06

---
+++ main.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your paragraph and to count the words!"
+    }
+]);
+const words = answer.Sentence.trim().split(" ");
+//print the array of word to the console....
+console.log(words);
+//print the word count of the sentence  to the console....
+console.log(`the total word are ${words.length}`);
+console.log('The End');
